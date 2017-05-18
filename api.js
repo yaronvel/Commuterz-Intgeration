6322b7a71d4cacca2a1ac39174275110b0650d25
Yaron Velner <yaron172@yahoo.com>
2017-05-18 10:53:19 +0300

    request a ride api

--- api.js
+++ api.js
@@ -11,8 +11,6 @@ module.exports.getRiderBalance = function ( callback ) {
             callback(err,parseInt(result.toString(10)));
         });
     });
-    //var tokenInstance = common.getTokenInstance();
-    //return parseInt(tokenInstance.balanceOf(account).toString(10));
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -28,9 +26,45 @@ module.exports.getRiderIPFSLink = function ( callback ) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-module.exports.getLotteryBalance = function ( ) {
-    var account = common.getLotteryAddress();
-    var tokenInstance = common.getTokenInstance();
-    
-    return parseInt(tokenInstance.balanceOf(account).toString(10));
+module.exports.riderPaysContract = function ( rideCost, callback ) {
+    var account = common.getRiderAccount();
+
+    var account = common.getRiderAccount();
+    common.getTokenInstance(function(err,result){
+        tokenInstance = result;
+        if( err ) return callback(err, result);
+        tokenInstance.approve(common.getCommuterzAddress(), rideCost,{from:account,gas:500000}, function(err,result){
+            if( err ) return callback(err,result);
+            callback(err,result);
+        });
+    });        
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+module.exports.getNextRideId = function ( callback ) {
+    var account = common.getRiderAccount();
+    common.getCommuterzInstance().getRideId(account, callback );    
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+module.exports.passangerRequestARide = function (rideCost, callback ) {
+    var account = common.getRiderAccount();
+    common.getCommuterzInstance().passangerRideRequest(rideCost, {from:account,gas:500000}, callback );    
+};
+ 
+//////////////////////////////////////////////////////////////////////////////// 
+ 
+ 
+
+////////////////////////////////////////////////////////////////////////////////
+
+module.exports.txStatus = function ( txhash, callback ) {
+    var web3 = common.getWeb3();
+    web3.eth.getTransactionReceipt(txhash, function(err,result){
+       if( err ) return callback(err,false);
+       callback(err,result !== null ); 
+    });
 };
--- testapi.js
+++ testapi.js
@@ -1,5 +1,58 @@
 var api = require('./api.js');
 
+
+api.txStatus('0x31b04a7aca793c699c3ae5b1dfe6dfc8ad96dc13fa78748043881f437235bd3f',function(err,result){
+    console.log(err,result);
+});
+
+
+
+api.passangerRequestARide(39,function(err,result){
+    if( err ) console.log(err);
+    else {
+        tx = result;
+        console.log(tx);
+        api.txStatus( tx, function(err,result){
+           if( err ) console.log(err);
+           tx_confirmed = result;
+           if( tx_confirmed ) {
+               console.log("confirmed")
+           }
+           else {
+               console.log("not confirmed");
+           }
+        });
+    }
+});
+
+
+
+api.getNextRideId( function(err,result){
+   console.log(err,result); 
+   var id = result;
+});
+
+
+api.riderPaysContract(39,function(err,result){
+    if( err ) console.log(err);
+    else {
+        tx = result;
+        console.log(tx);
+        api.txStatus( tx, function(err,result){
+           if( err ) console.log(err);
+           tx_confirmed = result;
+           if( tx_confirmed ) {
+               console.log("confirmed")
+           }
+           else {
+               console.log("not confirmed");
+           }
+        });
+    }
+});
+
+
+/*
 api.getRiderBalance(function(err,result){
    console.log(err,result);    
 });
@@ -7,4 +60,4 @@ api.getRiderBalance(function(err,result){
 
 api.getRiderIPFSLink(function(err,result){
    console.log(err,result);    
-});
+});*/
