2b42eb816db412d6b31ac4be6c83a9b938c1049b
Yaron Velner <yaron172@yahoo.com>
2017-06-12 13:48:33 +0300

    just to be sure

--- api.js
+++ api.js
@@ -214,3 +214,4 @@ module.exports.winnerCollectPrize = function ( winnerPrivateKey, callback ) {
     });    
 };
 
+                                                                                                                                                                                                        
